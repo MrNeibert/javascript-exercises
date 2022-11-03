164529b1a0e74f5273bfb460a300f58b719290ef
Willian <willian.neibertz@gmail.com>
2022-11-02 21:39:45 -0300

    Done exercises 01-03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, repeat) {
 
-};
+  let stringarray = new Array()
+  if (repeat < 0) {
+    return "ERROR";
+  } else {
+    for (let i= 0; i < repeat; i++) {
+       stringarray.push(string);} }
+ 
+  text = stringarray.join("");
+  return text;
+    }
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(text) {;
+  let string = text.toString();
+  let size = string.length;
+  let reversed = new Array();
 
-};
+  for (let i =0; i <= size; i++){
+   reversed.push(string.charAt(size -i)); }
+ 
+  return reversed.join("")}
 
 // Do not edit below this line
 module.exports = reverseString;
